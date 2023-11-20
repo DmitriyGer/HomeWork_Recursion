4fe227f7731a70206c53db99e7c32d126393ab95
Dmitriy <dim1a15@mail.ru>
2023-11-20 10:51:06 +0300

    Решение всех задач

--- H_W_Recursoin/Program.cs
+++ H_W_Recursoin/Program.cs
@@ -36,4 +36,26 @@ SummNatural(1, 15, 0);
 
 
 
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
 
+/*
+int FAckermana(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return FAckermana(m - 1, 1);
+    else return FAckermana(m - 1, FAckermana(m, n - 1));
+}
+
+System.Console.Write("Inpur M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = FAckermana(m, n);
+
+System.Console.WriteLine();
+System.Console.WriteLine("Answer: ");
+System.Console.Write(result);
+*/
